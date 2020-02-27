1d862c6136fad6373c7c6604c6bab7de8bdbf2a9
ENvironmentSet <sbshw1@gmail.com>
2020-02-28 08:49:10 +0900

    <feat>(overcurried): Change hook where AppEnvironment is applied
    I used wrapPageElement before, because of gatsby's bug that I can't use Static query in wrapRootElement. but since it fixed, I changed hook where AppEnvironment is applied to wrapRootElement which is more proper one.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,10 +6,8 @@ import React from 'react';
 
 import AppEnvironment from 'components/templates/AppEnvironment';
 
-export const wrapPageElement = ({ element }) => {
-  return (
-    <AppEnvironment>
-      {element}
-    </AppEnvironment>
-  );
-};
+export const wrapRootElement = ({ element }) => (
+  <AppEnvironment>
+    {element}
+  </AppEnvironment>
+);
