b43f2ec578eb2c1d2f4f9b4d1bb8e15fe0490403
ENvironmentSet <sbshw1@gmail.com>
2020-03-03 14:54:28 +0900

    revert patch

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,1 +1,33 @@
-export * from 'components/pages/404';
+import * as React from 'react';
+import SEO from 'components/templates/SEO';
+import HyperLink from 'components/atoms/HyperLink';
+import { Text, H1Text } from 'components/atoms/Text';
+import Accent from 'components/atoms/Accent';
+
+export default function NotFoundPage() {
+  function NotFoundMessage() {
+    return (
+      <>
+        <H1Text>Not Found</H1Text>
+        <Text>
+          Oops, My route function has produced
+          {' '}
+          <Accent>
+            <HyperLink to='https://wiki.haskell.org/Bottom'>⊥</HyperLink>
+          </Accent>,
+          You should check your <HyperLink to='https://en.wikipedia.org/wiki/Uniform_Resource_Identifier'>URI</HyperLink>.
+        </Text>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <SEO
+        title='404: Not Found'
+        description='Oops, My route function has produced ⊥, You should check your URI'
+      />
+      <NotFoundMessage />
+    </>
+  );
+};
