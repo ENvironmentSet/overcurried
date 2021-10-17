5d2ca87f6d94ea676e7db8c44d7f9aef13300dd5
ENvironmentSet <sbshw1@gmail.com>
2021-10-18 02:39:34 +0900

    add style for summary tag

--- src/components/atoms/Markdown.js
+++ src/components/atoms/Markdown.js
@@ -8,7 +8,7 @@ export default function Markdown({ children }) {
       a {
         color: ${Theme.pickColor('hyperLink')};
       }
-      p, span, small, h1, h2, h3, h4, h5, h6, li, th, td {
+      p, span, small, h1, h2, h3, h4, h5, h6, li, th, td, summary {
         color: ${Theme.pickColor('foreground')};
       }
       strong {
